69d939c178a6e24287556800a367996bb3430e02
Mariangeles <mari_riveraper@live.com.mx>
2023-03-28 00:48:33 -0600

    create dist

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://mariangelescode.github.io/profile-react'
+  base: 'https://mariangelescode.github.io/my-profile'
 
 })
